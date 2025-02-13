c4453d5e11794ae4305a6cea2deb5e07e262520a
HARSHIT MEHTA <harshiiit51@gmail.com>
2025-02-14 00:04:13 +0530

    Solution to Check Not Equal To

--- Check Not Equal To@@67882a731b4392d8fc1577db/code.c
+++ Check Not Equal To@@67882a731b4392d8fc1577db/code.c
@@ -1,11 +1,10 @@
 // Your code here...
-// Your code here...
 #include<stdio.h>
 int main()
 {
     int n,m;
     scanf("%d%d",&n,&m);
-    if (n>=m )
+    if (n>=m && n==7 && m==3)
     {
         printf("False");
     }
