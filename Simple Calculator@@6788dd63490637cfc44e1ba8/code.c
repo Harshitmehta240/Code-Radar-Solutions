272d8905fbe2a5e586fb58bfddb1152868055a64
HARSHIT MEHTA <harshiiit51@gmail.com>
2025-02-15 19:57:30 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,14 +4,8 @@
 int main() {
     double num1, num2;
     char operator;
-
-    printf("Enter first number: ");
     scanf("%lf", &num1);
-
-    printf("Enter an operator (+, -, *, /): ");
     scanf(" %c", &operator);
-
-    printf("Enter second number: ");
     scanf("%lf", &num2);
 
     if (operator == '+') {
