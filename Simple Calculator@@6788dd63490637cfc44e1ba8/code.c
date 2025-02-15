ba822ccc485d93d1a5e153159f1f0049cb424a54
HARSHIT MEHTA <harshiiit51@gmail.com>
2025-02-15 19:58:51 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,24 +1,23 @@
-// Your code here...
 #include <stdio.h>
 
 int main() {
     double num1, num2;
     char operator;
     scanf("%lf", &num1);
-    scanf(" %c", &operator);
+    scanf(" %c", &operator);  // Added a space before %c to handle any leading whitespace
     scanf("%lf", &num2);
 
     if (operator == '+') {
-        printf("%.2lf\n", num1 + num2);
+        printf("%.2lf\n", num1 + num2);  // Newline after the result
     } else if (operator == '-') {
-        printf("%.2lf", num1 - num2);
+        printf("%.2lf\n", num1 - num2);  // Newline after the result
     } else if (operator == '*') {
-        printf("%.2lf", num1 * num2);
+        printf("%.2lf\n", num1 * num2);  // Newline after the result
     } else if (operator == '/') {
         if (num2 != 0) {
-            printf("%.2lf", num1 / num2);
+            printf("%.2lf\n", num1 / num2);  // Newline after the result
         } else {
-            printf("Error");
+            printf("Error\n");  // Added newline after error
         }
     } 
 
